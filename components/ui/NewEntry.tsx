da958b388b0583b1bac3486c7cde8ac8a08b0160
Daniel Unapanta <dunapanta@outlook.es>
2022-05-12 21:14:52 -0500

    Add form functionality

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import AddIcon from "@mui/icons-material/Add";
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [hasTouched, setHasTouched] = useState(false);
+
+  const onChangeTextHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onSaveHandler = () => {
+    if (inputValue.length === 0) return;
+    console.log("Saving", inputValue);
+  };
+
   return (
     <Box sx={{ marginBottom: 2, padding: 2 }}>
-      <Button variant="contained" fullWidth startIcon={<AddIcon />}>
-        Nueva Tarea
-      </Button>
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder="Nueva Tarea"
-        autoFocus
-        multiline
-        label="Nueva entrada"
-        helperText="Ingrese una nueva tarea"
-      />
-      <Box display="flex" justifyContent="space-evenly">
-        <Button variant="outlined" endIcon={<SaveIcon />}>
-          Guardar
+      {isAdding ? (
+        <>
+          {" "}
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder="Nueva Tarea"
+            autoFocus
+            multiline
+            label="Nueva entrada"
+            helperText={
+              inputValue.length <= 0 && hasTouched && "Ingrese un valor"
+            }
+            value={inputValue}
+            error={inputValue.length <= 0 && hasTouched}
+            onChange={onChangeTextHandler}
+            onBlur={() => setHasTouched(true)}
+          />
+          <Box display="flex" justifyContent="space-evenly">
+            <Button
+              onClick={onSaveHandler}
+              variant="outlined"
+              endIcon={<SaveIcon />}
+            >
+              Guardar
+            </Button>
+            <Button
+              color="secondary"
+              variant="outlined"
+              onClick={() => setIsAdding(false)}
+              endIcon={<SaveIcon />}
+            >
+              Cancelar
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Button
+          variant="contained"
+          onClick={() => setIsAdding(true)}
+          fullWidth
+          startIcon={<AddIcon />}
+        >
+          Nueva Tarea
         </Button>
-        <Button color="secondary" variant="outlined" endIcon={<SaveIcon />}>
-          Cancelar
-        </Button>
-      </Box>
+      )}
     </Box>
   );
 };
